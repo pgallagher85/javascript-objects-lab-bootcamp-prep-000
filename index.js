3465b11677de1de04cda5927338122521ce30784
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-06 13:32:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var recipes = {
   blahblah: 'yoyo'
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object.assign({}, object, {[key]: value}
+  return object
+}
+
  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
